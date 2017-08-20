5825f39ae804dd5442625cb9899cc1f466a19a1c
Arnar Freyr Helgason <nozagleh@gmail.com>
2017-08-20 11:38:30 +0200

    Add layout land files

---
+++ app/src/main/java/com/example/arnarfreyr/dicegame3000/CalculateScore.java
@@ -0,0 +1,8 @@
+package com.example.arnarfreyr.dicegame3000;
+
+/**
+ * Created by arnarfreyr on 20.8.2017.
+ */
+
+public class CalculateScore {
+}
---
+++ app/src/main/java/com/example/arnarfreyr/dicegame3000/SQLManager.java
@@ -0,0 +1,8 @@
+package com.example.arnarfreyr.dicegame3000;
+
+/**
+ * Created by arnarfreyr on 19.8.2017.
+ */
+
+public class SQLManager {
+}
